bc8d373754f74faa710480efdec49e4e5ce72ca4
nimelord <nimelord@gmail.com>
2018-10-11 14:33:41 +0300

    Chapter #2: Task 1.

---
+++ src/main/scala/T2_1_Sign.scala
@@ -0,0 +1,21 @@
+import T2_1_Sign._
+
+import scala.util.Random
+
+object T2_1_Sign {
+
+  def sign(value: Int): Int = if (value > -1) 1 else -1;
+
+  def apply() : T2_1_Sign = new T2_1_Sign
+}
+
+
+class T2_1_Sign {
+
+  val i = new Random(System.currentTimeMillis()).nextInt();
+
+  def perform() {
+    println(f"Sign of ${i} is: ${sign(i)}")
+  }
+
+}
---
+++ src/main/scala/TestTasks.scala
@@ -0,0 +1,11 @@
+
+object TestTasks {
+
+  def main(args: Array[String]) {
+
+    val str = "Welcome to the Scala world!"
+    println(str)
+
+    T2_1_Sign().perform
+  }
+}

cd9324a92cee383c2ca5376e0f3cfe76291aed67
nimelord <nimelord@gmail.com>
2018-10-12 17:43:00 +0300

    Chapter #2: Task 11.

---
+++ src/test/scala/T2_11_DateInterpolationSpec.scala
@@ -0,0 +1,13 @@
+import java.time.LocalDate
+
+import org.scalatest.FlatSpec
+
+class T2_11_DateInterpolationSpec extends FlatSpec{
+
+	"Date interpolation" should "return expect format of date" in {
+
+		val date = LocalDate.of(2018, 1, 2)
+		val strDate = T2_11_DateInterpolation.format(date)
+		assert(strDate == "02-01-2018", s"unexpected format: $strDate")
+	}
+}
